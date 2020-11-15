b04a8bd473a8207e9796153e7bde8617cf05282b
Kaye Engbino <engbinop@gmail.com>
2020-11-15 18:31:54 +0800

    To function

--- Quadratic Equation/quadraticEquation.c
+++ Quadratic Equation/quadraticEquation.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <math.h>
 
+void quadraticEq(double root1, double root2, int a, int b, int c, int d)
+{
+  d = pow(b, b) - 4*a*c;
+
+  //Complex roots
+  if (d < 0) 
+    printf("COMPLEX");
+
+  //Real roots
+  else 
+  {
+    root1 = (-b + sqrt(d))/(2*a);
+    root2 = (-b - sqrt(d))/(2*a);   
+
+    printf("%.4lf, %.4lf", root1, root2);
+  }
+}
+
 int main()
 {
   int a, b, c, d;
@@ -10,22 +28,16 @@ int main()
   {
     printf("\nCoefficients: ");
     scanf("%d", &a);
-    if(a == 0)  //Terminates if input is 0
+
+    //Terminates if input is 0
+    if(a == 0)  
       return 0;
     scanf("%d", &b);
     scanf("%d", &c);
           
-    d = pow(b, b) - 4*a*c;
-
-    if (d < 0) //Complex roots
-      printf("COMPLEX");
-    else //Real roots
-    {
-      root1 = (-b + sqrt(d))/(2*a);
-      root2 = (-b - sqrt(d))/(2*a);
-
-      printf("%.4lf, %.4lf", root1, root2);    
-    }
+    quadraticEq(root1, root2, a, b, c, d);
   }
   while(a != 0);
+
+  return 0;
 }
